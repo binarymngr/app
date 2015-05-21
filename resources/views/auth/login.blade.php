fc7a4c566b2b09a4b331f5a7127e1455bba9d0af
Michel Käser <mail@michelkaeser.ch>
2015-05-21 23:00:00 +0200

    Use Lumen's built-in multilang support for internationalization.
    Summary: Title says it all.
    
    Reviewers: michelkaeser
    
    Reviewed By: michelkaeser
    
    Subscribers: michelkaeser
    
    Projects: #binarymngr
    
    Differential Revision: https://phabricator.michelkaeser.ch/D3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>BinaryMngr - Login</title>
+    <title>BinaryMngr - @lang('login.title')</title>
     <link rel="shortcut icon" href="/favicon.ico">
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
@@ -21,20 +21,20 @@
         <div class="row">
             <div class="col-sm-12">
                 <div id="brand">
-                    <h1>Login</h1>
+                    <h1>@lang('login.title')</h1>
                 </div>
             </div>
             <div class="col-sm-7 col-md-6 col-lg-5 login">
                 <form action="/auth/login" method="POST" class="form-horizontal" role="form">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <div class="form-group">
-                        <label for="email" class="col-sm-2 col-md-2 control-label">Email</label>
+                        <label for="email" class="col-sm-2 col-md-2 control-label">@lang('login.email')</label>
                         <div class="col-sm-10 col-md-10">
                             <input type="email" class="form-control" id="email" name="email" placeholder="email@domain.tld" value="{{ Session::get('email', '') }}" tabindex="1" required>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="password" class="col-sm-2 col-md-2 control-label">Password</label>
+                        <label for="password" class="col-sm-2 col-md-2 control-label">@lang('login.password')</label>
                         <div class="col-sm-10 col-md-10">
                             <input type="password" class="form-control" id="password" name="password" placeholder="******" tabindex="2" required>
                         </div>
@@ -43,31 +43,31 @@
                         <div class="col-xs-8 col-sm-offset-2 col-sm-6 col-md-offset-2 col-md-6">
                             <div class="checkbox">
                                 <label>
-                                    <input tabindex="3" type="checkbox" name="remember"> Remember Email
+                                    <input tabindex="3" type="checkbox" name="remember"> @lang('login.remember-email')
                                 </label>
                             </div>
                         </div>
                         <div class="col-xs-4 col-sm-4 col-md-4 submit">
-                            <button type="submit" class="btn btn-primary btn-lg" tabindex="4">Log In</button>
+                            <button type="submit" class="btn btn-primary btn-lg" tabindex="4">@lang('login.login')</button>
                         </div>
                     </div>
                 </form>
             </div>
             <div class="col-sm-5 col-md-6 col-lg-7 details">
-                <p><strong>Welcome to BinaryMngr!</strong><br>
-                    Enjoy the new way of managing self-compiled binaries.</p>
+                <p><strong>@lang('login.welcome')</strong><br>
+                    @lang('login.intro')</p>
                     @if (Session::get('login_failed'))
                         <p style="color: red"><br>
                             <span class="pficon-layered">
                                 <span class="pficon pficon-error-octagon"></span>
                                 <span class="pficon pficon-error-exclamation"></span>
                             </span>
-                            Invalid credentials. Please try again.
+                            @lang('login.invalid-credentials')
                         </p>
                     @elseif (Session::get('logged_out'))
                         <p style="color: #439b3b"><br>
                             <span class="pficon pficon-ok"></span>
-                            You logged out successfully.
+                            @lang('login.logged-out')
                         </p>
                     @endif
             </div>
